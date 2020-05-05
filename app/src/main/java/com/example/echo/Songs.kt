e4489cb6e61cafd222123e7e28ddf17e6d20a042
Ankit Singh <singhanki67@gmail.com>
2020-05-05 12:16:55 +0530

    Echo App

--- app/src/main/java/com/example/echo/Songs.kt
+++ app/src/main/java/com/example/echo/Songs.kt
@@ -6,8 +6,21 @@ import android.os.Parcelable.Creator
 
 class Songs(var songID: Long,var songTitle: String,var artist: String,var songData: String,var dateAdded: Long) : Parcelable
 {
-    override fun writeToParcel(dest: Parcel?, flags: Int) {
+    constructor(parcel: Parcel) : this(
+        parcel.readLong(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readLong()
+    ) {
+    }
 
+    override fun writeToParcel(dest: Parcel?, flags: Int) {
+        dest!!.writeLong(songID)
+        dest.writeString(songTitle)
+        dest.writeString(artist)
+        dest.writeString(songData)
+        dest.writeLong(dateAdded)
     }
 
     override fun describeContents(): Int {
@@ -23,6 +36,16 @@ class Songs(var songID: Long,var songTitle: String,var artist: String,var songDa
             val songOne = song1.dateAdded.toDouble()
             val songTwo = song2.dateAdded.toDouble()
             songTwo.compareTo(songOne)
+        }
+    }
+
+    companion object CREATOR : Creator<Songs> {
+        override fun createFromParcel(parcel: Parcel): Songs {
+            return Songs(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Songs?> {
+            return arrayOfNulls(size)
         }
     }
 
